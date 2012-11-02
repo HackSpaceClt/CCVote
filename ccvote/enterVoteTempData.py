13394d6ab2b83773c56947eeae8303f5b9a4a660
Paul Wheeler <paul@paulwheeler.com>
2012-11-01 22:25:59 -0400

    Fixed enterVoteTempData
    I made the scren run, though I can't image that does what we want it to
    since it just stored the user's id in all the fields

--- ccvote/enterVoteTempData.py
+++ ccvote/enterVoteTempData.py
@@ -1,7 +1,7 @@
 from main.models import *
 
 for each in range(1,13):
-    p = VoteTemp(motion_id = MotionData.objects.get(motion_id = 1).motion_id, user_id = UserData.objects.get(user_id = each).user_id, user_name = UserData.objects.get(user_id = each).user_name, user_status = UserData.objects.get(user_id = each).user_status, vote = 'pro')
+    p = VoteTemp(motion_id = MotionData.objects.get(motion_id = 1), user_id = UserData.objects.get(user_id = each), user_name = UserData.objects.get(user_id = each), user_status = UserData.objects.get(user_id = each), vote = 'pro')
     p.save()
 
 
