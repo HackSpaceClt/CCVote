3c999322ecb0326715045313e17e32c316217945
David Durham <david@debian.localnet>
2012-12-13 00:02:37 -0500

    committing because git wants me to for some reason relating to merge conflicts in settings.py?

--- ccvote/ccvote/settings.py
+++ ccvote/ccvote/settings.py
@@ -103,7 +103,7 @@ ROOT_URLCONF = 'ccvote.urls'
 WSGI_APPLICATION = 'ccvote.wsgi.application'
 
 TEMPLATE_DIRS = (
-    '/Users/online/CCVote/ccvote/templates/'
+    '/home/david/CCVote8BackupCopy/ccvote/templates/'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
